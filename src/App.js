60a5b7c718bf095fc7547ef087bcac7ee1934d7c
manq2010 <manq2010@gmail.com>
2023-01-01 18:54:14 +0200

    Add about section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { ThemeContext } from './contexts/theme';
 import './App.css';
 import Footer from './features/Footer/Footer';
 import Header from './features/Header/Header';
+import About from './features/About/About';
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext);
   return (
     <div id="top" className={`${themeName} app`}>
       <Header />
-
+      <main>
+        <About />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/features/About/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { about } from '../../data/portfolio';
+
+const AboutWrapper = styled.div`
+
+`;
+
+const About = () => {
+  const {
+    name, role, description, button,
+  } = about;
+  return (
+    <AboutWrapper>
+      { name && (
+        <div>
+          <h3>
+            HI, MY NAME IS
+          </h3>
+          <h1>
+            {name}
+            .
+          </h1>
+
+        </div>
+
+      )}
+      { role && (
+        <div>
+          <h1>
+            I am a
+            {' '}
+            {role}
+            .
+          </h1>
+        </div>
+
+      )}
+
+      { description && (
+        <div>
+          <h3>
+            {description}
+          </h3>
+        </div>
+      )}
+
+      { button && (
+      <button type="button">
+        {button}
+      </button>
+      )}
+    </AboutWrapper>
+  );
+};
+
+export default About;
