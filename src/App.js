6f6ff511aa01a2e5aa9ead69ac153707881dcbf9
manq2010 <manq2010@gmail.com>
2023-01-02 17:33:55 +0200

    Add scroll up function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import Main from './pages/Main/Main';
 import ProjectPage from './pages/Project/ProjectPage';
+import ScrollToTop from './features/ScrollToTop/ScrollToTop';
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext);
   return (
     <div id="top" className={`${themeName} app`}>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/projects" exact element={<ProjectPage />} />
