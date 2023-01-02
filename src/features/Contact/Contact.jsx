1be31bfcc0998f057adbff0c94c79f1cff9e09d8
manq2010 <manq2010@gmail.com>
2023-01-02 17:14:25 +0200

    Remove form from Contact

--- src/features/Contact/Contact.jsx
+++ src/features/Contact/Contact.jsx
@@ -1,11 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Snackbar, SnackbarContent, IconButton } from '@mui/material';
-import { isEmail } from 'validator/lib/isEmail';
+import React from 'react';
 import styled from 'styled-components';
-import SendIcon from '@mui/icons-material/Send';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CloseIcon from '@mui/icons-material/Close';
 // import EmailIcon from '@mui/icons-material/Email';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -18,60 +12,9 @@ const ContactWrapper = styled.section`
     flex-direction: column;
 `;
 
-const Contact = () => {
-  const [open, setOpen] = useState(false);
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const [success, setSuccess] = useState(false);
-  const [errMsg, setErrMsg] = useState('');
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
-
-  const handleContactForm = (e) => {
-    e.preventDefault();
-
-    if (name && email && message) {
-      if (isEmail(email)) {
-        const responseData = {
-          name,
-          email,
-          message,
-        };
-
-        axios.post(contactsData.sheetAPI, responseData).then((res) => {
-          console.log(res, 'success');
-          console.log(success, 'success');
-          setSuccess(true);
-          setErrMsg('');
-
-          setName('');
-          setEmail('');
-          setMessage('');
-          setOpen(false);
-          console.log(open, 'open');
-        });
-      } else {
-        setErrMsg('Invalid email');
-        console.log(errMsg, 'Invalid email');
-        setOpen(true);
-      }
-    } else {
-      setErrMsg('Enter all the fields');
-      setOpen(true);
-    }
-  };
-
-  return (
-    <div>
-      {
+const Contact = () => (
+  <div>
+    {
       contactsData && (
       <ContactWrapper>
         <h1>Contacts</h1>
@@ -81,109 +24,6 @@ const Contact = () => {
             <EmailIcon />
           </span>
         </a> */}
-
-        <div className="contacts-form">
-          <form onSubmit={handleContactForm}>
-            <div className="input-container">
-              <label htmlFor="Name">
-                Name
-                <input
-                  placeholder="John Doe"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  type="text"
-                  name="Name"
-                />
-              </label>
-            </div>
-            <div className="input-container">
-              <label
-                htmlFor="Email"
-              >
-                Email
-                <input
-                  placeholder="John@doe.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  name="Email"
-                />
-              </label>
-            </div>
-            <div className="input-container">
-              <label
-                htmlFor="Message"
-              >
-                Message
-                <textarea
-                  placeholder="Type your message...."
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  type="text"
-                  name="Message"
-                />
-              </label>
-            </div>
-            <div className="submit-btn">
-              <button
-                type="submit"
-              >
-                <p>{!success ? 'Send' : 'Sent'}</p>
-                <div className="submit-icon">
-                  <SendIcon
-                    className="send-icon"
-                    style={{
-                      animation: !success
-                        ? 'initial'
-                        : 'fly 0.8s linear both',
-                      position: success
-                        ? 'absolute'
-                        : 'initial',
-                    }}
-                  />
-                  <CheckCircleIcon
-                    className="success-icon"
-                    style={{
-                      display: !success
-                        ? 'none'
-                        : 'inline-flex',
-                      opacity: !success ? '0' : '1',
-                    }}
-                  />
-                </div>
-              </button>
-            </div>
-          </form>
-
-          <Snackbar
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-            open={open}
-            autoHideDuration={4000}
-            onClose={handleClose}
-          >
-            <SnackbarContent
-              action={(
-                <>
-                  <IconButton
-                    size="small"
-                    aria-label="close"
-                    color="inherit"
-                    onClick={handleClose}
-                  >
-                    <CloseIcon fontSize="small" />
-                  </IconButton>
-                </>
-              )}
-              style={{
-                fontFamily: 'var(--primaryFont)',
-              }}
-              message={errMsg}
-            />
-          </Snackbar>
-        </div>
 
         <div>
           {/* <a href={contactsData.linkedin} target="_blank" rel="noreferrer">
@@ -225,8 +65,7 @@ const Contact = () => {
       </ContactWrapper>
       )
 }
-    </div>
-  );
-};
+  </div>
+);
 
 export default Contact;
