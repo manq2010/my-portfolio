5e282a91f742b51b07ec09631eb5500c92a425c3
manq2010 <manq2010@gmail.com>
2023-01-02 16:09:44 +0200

    Add project page

--- src/App.js
+++ src/App.js
@@ -4,41 +4,16 @@ import { ThemeContext } from './contexts/theme';
 import './App.css';
 
 import Main from './pages/Main/Main';
-
-// import Footer from './features/Footer/Footer';
-// import Header from './features/Header/Header';
-// import About from './features/About/About';
-// import Projects from './features/Projects/Projects';
-// import Skills from './features/Skills/Skills';
-// import Contact from './features/Contact/Contact';
-// import Testimony from './features/Testimony/Testimony';
-// import Experience from './features/Experience/Experience';
-// import ScrollToTop from './features/ScrollToTop/ScrollToTop';
+import ProjectPage from './pages/Project/ProjectPage';
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext);
   return (
     <div id="top" className={`${themeName} app`}>
       <Router>
-
-        {/* <Header />
-
-      <main>
-        <About />
-        <Projects />
-        <Experience />
-        <Skills />
-        <Testimony />
-        <Contact />
-      </main>
-
-      <ScrollToTop />
-      <Footer /> */}
         <Routes>
           <Route path="/" element={<Main />} />
-          {/* <Route path="/projects" component={Projects} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/experience" component={Experience} /> */}
+          <Route path="/projects" exact element={<ProjectPage />} />
         </Routes>
       </Router>
     </div>
