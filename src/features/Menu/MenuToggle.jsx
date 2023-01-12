7f635f1bf71ed41e05f895418f0160e8c76ff1a2
manq2010 <manq2010@gmail.com>
2023-01-12 06:57:35 +0200

    Add useOnClickOutside hook

--- src/features/Menu/MenuToggle.jsx
+++ src/features/Menu/MenuToggle.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import keyCode from '../../utils/keyCode';
 import links from '../../data/navlinksData';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 const NenuWrapper = styled.div`
   display: block;
@@ -233,33 +234,6 @@ const MenuToggle = () => {
       window.removeEventListener('resize', onResize);
     };
   }, []);
-
-  const useOnClickOutside = (ref, handler) => {
-    useEffect(
-      () => {
-        const listener = (event) => {
-          // Do nothing if clicking ref's element or descendent elements
-          if (!ref.current || ref.current.contains(event.target)) {
-            return;
-          }
-          handler(event);
-        };
-        document.addEventListener('mousedown', listener);
-        document.addEventListener('touchstart', listener);
-        return () => {
-          document.removeEventListener('mousedown', listener);
-          document.removeEventListener('touchstart', listener);
-        };
-      },
-      // Add ref and handler to effect dependencies
-      // It's worth noting that because passed in handler is a new ...
-      // ... function on every render that will cause this effect ...
-      // ... callback/cleanup to run every render. It's not a big deal ...
-      // ... but to optimize you can wrap handler in useCallback before ...
-      // ... passing it into this hook.
-      [ref, handler],
-    );
-  };
 
   const wrapperRef = useRef();
   useOnClickOutside(wrapperRef, () => setMenuOpen(false));
